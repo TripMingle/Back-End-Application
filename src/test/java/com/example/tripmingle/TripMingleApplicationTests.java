1153a5ddf192fde44265c4bd1997d83a6e7b22a5
thdgustjd1 <thdgustjd1@naver.com>
2024-06-04 19:05:17 +0900

    first commit

---
+++ src/test/java/com/example/tripmingle/TripMingleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tripmingle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripMingleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
