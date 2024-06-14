bacad61e3698b240e55634577ef7f766cbacfbdb
Min Gyu <kimmingyu9811@gmail.com>
2024-06-14 12:26:51 +0900

    feat: Posting 엔티티 생성

---
+++ src/main/java/com/example/tripmingle/entity/Posting.java
@@ -0,0 +1,30 @@
+package com.example.tripmingle.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Posting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private PostingType postingType;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+}
