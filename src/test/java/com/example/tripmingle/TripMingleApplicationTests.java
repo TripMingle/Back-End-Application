7c61214d89fa0d1356d49c22bc409bcec9a0eff4
송현성 <129783824+thdgustjd1@users.noreply.github.com>
2024-07-02 18:17:45 +0900

    1234

--- src/test/java/com/example/tripmingle/TripMingleApplicationTests.java
+++ src/test/java/com/example/tripmingle/TripMingleApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.tripmingle;
+/*package com.example.tripmingle;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,3 +11,6 @@ class TripMingleApplicationTests {
     }
 
 }
+
+
+ */
