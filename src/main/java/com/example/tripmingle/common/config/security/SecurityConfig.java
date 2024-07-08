93fd657685aca4cd68eb8a7e58ef74ade8280b1e
송현성 <129783824+thdgustjd1@users.noreply.github.com>
2024-07-08 20:22:44 +0900

    [Feat/swm 154] cors 오류 해결 (#44)
    * [SWM-183]feat : 매칭서버 리팩토링 - 로직변경
    
    * [SWM-183]fix : JWT 토큰에 Bearer 추가
    
    * [SWM-183]feat : cors 에러 해결

--- src/main/java/com/example/tripmingle/common/config/security/SecurityConfig.java
+++ src/main/java/com/example/tripmingle/common/config/security/SecurityConfig.java
@@ -70,6 +70,8 @@ public class SecurityConfig {
 						CorsConfiguration configuration = new CorsConfiguration();
 
 						configuration.setAllowedOrigins(Collections.singletonList(allowedOrigins));
+						configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+
 						configuration.setAllowedMethods(Collections.singletonList("*"));
 						configuration.setAllowCredentials(true);
 						configuration.setAllowedHeaders(Collections.singletonList("*"));
