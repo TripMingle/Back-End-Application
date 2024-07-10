bcf52257dbda849d98f112446db8459ab5d6edcc
송현성 <129783824+thdgustjd1@users.noreply.github.com>
2024-07-10 20:27:34 +0900

    [SWM-117]fix : 동행글 삭제 시 연관된 동행확정들도 삭제되도록 수정

--- src/main/java/com/example/tripmingle/adapter/out/CompanionPersistAdapter.java
+++ src/main/java/com/example/tripmingle/adapter/out/CompanionPersistAdapter.java
@@ -1,5 +1,11 @@
 package com.example.tripmingle.adapter.out;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
 import com.example.tripmingle.common.error.ErrorCode;
 import com.example.tripmingle.common.exception.CompanionNotFound;
 import com.example.tripmingle.entity.Board;
@@ -7,47 +13,48 @@ import com.example.tripmingle.entity.Companion;
 import com.example.tripmingle.entity.User;
 import com.example.tripmingle.port.out.CompanionPersistPort;
 import com.example.tripmingle.repository.CompanionRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Component;
 
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class CompanionPersistAdapter implements CompanionPersistPort {
-    private final CompanionRepository companionRepository;
+	private final CompanionRepository companionRepository;
 
-    @Override
-    public void saveCompanion(Companion companion) {
-        companionRepository.save(companion);
-    }
+	@Override
+	public void saveCompanion(Companion companion) {
+		companionRepository.save(companion);
+	}
 
-    @Override
-    public Companion getCompanionByBoardAndUser(Board board, User currentUser) {
-        return companionRepository.findCompanionByBoardIdAndUserId(board.getId(),currentUser.getId())
-                .orElseThrow(()-> new CompanionNotFound("companion not found", ErrorCode.COMPANION_NOT_FOUND));
-    }
+	@Override
+	public Companion getCompanionByBoardAndUser(Board board, User currentUser) {
+		return companionRepository.findCompanionByBoardIdAndUserId(board.getId(), currentUser.getId())
+			.orElseThrow(() -> new CompanionNotFound("companion not found", ErrorCode.COMPANION_NOT_FOUND));
+	}
 
-    @Override
-    public void deleteCompanion(Companion companion) {
-        companion.delete();
-    }
+	@Override
+	public void deleteCompanion(Companion companion) {
+		companion.delete();
+	}
 
-    @Override
-    public List<Companion> getCompanionsByBoardId(Long boardId) {
-        return companionRepository.findCompanionsByBoardId(boardId);
-    }
+	@Override
+	public List<Companion> getCompanionsByBoardId(Long boardId) {
+		return companionRepository.findCompanionsByBoardId(boardId);
+	}
 
-    @Override
-    public Page<Companion> getCompanionsByUserId(Long userId, Pageable pageable) {
-        return companionRepository.findCompanionsByUserId(userId,pageable);
-    }
+	@Override
+	public Page<Companion> getCompanionsByUserId(Long userId, Pageable pageable) {
+		return companionRepository.findCompanionsByUserId(userId, pageable);
+	}
 
-    @Override
-    public boolean existsByUserIdAndBoardId(Long userId, Long boardId) {
-        return companionRepository.existsByUserIdAndBoardId(userId,boardId);
-    }
+	@Override
+	public boolean existsByUserIdAndBoardId(Long userId, Long boardId) {
+		return companionRepository.existsByUserIdAndBoardId(userId, boardId);
+	}
+
+	@Override
+	public void deleteCompanionByBoardId(Long boardId) {
+		companionRepository.findCompanionsByBoardId(boardId).forEach(companion -> companion.delete());
+	}
 
 }
--- src/main/java/com/example/tripmingle/application/service/CompanionService.java
+++ src/main/java/com/example/tripmingle/application/service/CompanionService.java
@@ -1,5 +1,11 @@
 package com.example.tripmingle.application.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
 import com.example.tripmingle.common.error.ErrorCode;
 import com.example.tripmingle.common.exception.LeaderCannotLeaveException;
 import com.example.tripmingle.common.utils.UserUtils;
@@ -8,63 +14,62 @@ import com.example.tripmingle.entity.Companion;
 import com.example.tripmingle.entity.Position;
 import com.example.tripmingle.entity.User;
 import com.example.tripmingle.port.out.CompanionPersistPort;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class CompanionService {
-    private final UserUtils userUtils;
-    private final CompanionPersistPort companionPersistPort;
+	private final UserUtils userUtils;
+	private final CompanionPersistPort companionPersistPort;
 
-    public void registerLeader(Board board, User user){
-        Companion companion = Companion.builder()
-                .user(user)
-                .board(board)
-                .position(Position.LEADER)
-                .build();
-        companionPersistPort.saveCompanion(companion);
-    }
+	public void registerLeader(Board board, User user) {
+		Companion companion = Companion.builder()
+			.user(user)
+			.board(board)
+			.position(Position.LEADER)
+			.build();
+		companionPersistPort.saveCompanion(companion);
+	}
 
-    public void confirmUsers(Board board, User currentUser, List<User> userList) {
-        userUtils.validateMasterUser(board.getUser().getId(),currentUser.getId());
+	public void confirmUsers(Board board, User currentUser, List<User> userList) {
+		userUtils.validateMasterUser(board.getUser().getId(), currentUser.getId());
 
-        userList.forEach(user ->{
-            if (!user.getId().equals(board.getUser().getId())) {
-                Companion companion = Companion.builder()
-                        .user(user)
-                        .board(board)
-                        .position(Position.MEMBER)
-                        .build();
-                companionPersistPort.saveCompanion(companion);
-            }
-        }
-        );
+		userList.forEach(user -> {
+				if (!user.getId().equals(board.getUser().getId())) {
+					Companion companion = Companion.builder()
+						.user(user)
+						.board(board)
+						.position(Position.MEMBER)
+						.build();
+					companionPersistPort.saveCompanion(companion);
+				}
+			}
+		);
 
-    }
+	}
 
+	public void leaveCompanion(Board board, User currentUser) {
+		Companion companion = companionPersistPort.getCompanionByBoardAndUser(board, currentUser);
+		if (companion.getPosition().equals(Position.LEADER)) {
+			throw new LeaderCannotLeaveException("leader can't leave.", ErrorCode.LEADER_CANT_LEAVE);
+		}
+		companionPersistPort.deleteCompanion(companion);
+	}
 
-    public void leaveCompanion(Board board, User currentUser) {
-        Companion companion = companionPersistPort.getCompanionByBoardAndUser(board,currentUser);
-        if(companion.getPosition().equals(Position.LEADER)){
-            throw new LeaderCannotLeaveException("leader can't leave.", ErrorCode.LEADER_CANT_LEAVE);
-        }
-        companionPersistPort.deleteCompanion(companion);
-    }
+	public List<Companion> getCompanionsByBoardId(Long boardId) {
+		return companionPersistPort.getCompanionsByBoardId(boardId);
+	}
 
-    public List<Companion> getCompanionsByBoardId(Long boardId) {
-        return companionPersistPort.getCompanionsByBoardId(boardId);
-    }
+	public Page<Companion> getCompanionsByUser(User currentUser, Pageable pageable) {
+		return companionPersistPort.getCompanionsByUserId(currentUser.getId(), pageable);
+	}
 
-    public Page<Companion> getCompanionsByUser(User currentUser, Pageable pageable) {
-        return companionPersistPort.getCompanionsByUserId(currentUser.getId(),pageable);
-    }
+	public boolean isParticipatingBoard(User currentUser, Board board) {
+		return companionPersistPort.existsByUserIdAndBoardId(currentUser.getId(), board.getId());
+	}
 
-    public boolean isParticipatingBoard(User currentUser, Board board) {
-        return companionPersistPort.existsByUserIdAndBoardId(currentUser.getId(),board.getId());
-    }
+	public void deleteCompanionByBoardId(Long boardId) {
+		companionPersistPort.deleteCompanionByBoardId(boardId);
+	}
 }
