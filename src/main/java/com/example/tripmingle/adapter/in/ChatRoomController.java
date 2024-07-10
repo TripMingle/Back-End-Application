de4fdfaeec839683acb8087d251aae703610ef96
mingyu <kimmingyu9811@gmail.com>
2024-07-10 14:14:04 +0900

    refactor: uri 수정

--- src/main/java/com/example/tripmingle/adapter/in/ChatRoomController.java
+++ src/main/java/com/example/tripmingle/adapter/in/ChatRoomController.java
@@ -16,13 +16,13 @@ public class ChatRoomController {
 
     private final ChatRoomUseCase chatRoomUseCase;
 
-    @PostMapping("/one-on-one")
+    @PostMapping("/one-on-one/enter")
     public ResponseEntity<ResultResponse> enterOneOnOneChatRoom(@RequestBody EnterOneOnOneChatRoomReqDTO enterOneOnOneChatRoomReqDTO) {
         EnterOneOnOneChatRoomResDTO enterOneOnOneChatRoomResDTO = chatRoomUseCase.enterOneOnOneChatRoom(enterOneOnOneChatRoomReqDTO);
         return ResponseEntity.ok(ResultResponse.of(ResultCode.CREATE_ONE_ON_ONE_CHAT_ROOM_SUCCESS, enterOneOnOneChatRoomResDTO));
     }
 
-    @PostMapping("/group")
+    @PostMapping("/group/enter")
     public ResponseEntity<ResultResponse> enterGroupChatRoom(@RequestBody EnterGroupChatRoomReqDTO enterGroupChatRoomReqDTO) {
         EnterGroupChatRoomResDTO enterGroupChatRoomResDTO = chatRoomUseCase.enterGroupChatRoom(enterGroupChatRoomReqDTO);
         return ResponseEntity.ok(ResultResponse.of(ResultCode.USER_ENTER_CHAT_ROOM_SUCCESS, enterGroupChatRoomResDTO));
