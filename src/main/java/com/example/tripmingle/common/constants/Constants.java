54edee4566e32953a320ba77d5ee05e22165fe94
송현성 <129783824+thdgustjd1@users.noreply.github.com>
2024-07-16 21:28:27 +0900

    [SWM-203]fix : 최근 게시물 조회 시 4개만 리턴하도록 수정

--- src/main/java/com/example/tripmingle/common/constants/Constants.java
+++ src/main/java/com/example/tripmingle/common/constants/Constants.java
@@ -3,6 +3,7 @@ package com.example.tripmingle.common.constants;
 public class Constants {
 
 	public static int PAGE_SIZE = 16;
+	public static int RECENT_PAGE_SIZE = 4;
 	public static int USER_MATCHING_SIZE = 5;
 	public static String SORT_CREATING_CRITERIA = "createdAt";
 	public static String SORT_ID_CRITERIA = "id";
