49605b782fab1795378bc4275a8e5f5981b4689e
mingyu <kimmingyu9811@gmail.com>
2024-07-17 15:39:14 +0900

    feature health check api for cicd test

--- src/main/java/com/example/tripmingle/adapter/in/HealthController.java
+++ src/main/java/com/example/tripmingle/adapter/in/HealthController.java
@@ -1,6 +1,5 @@
 package com.example.tripmingle.adapter.in;
 
-import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +16,12 @@ public class HealthController {
 	public String healthCheck() {
 		String email = SecurityContextHolder.getContext().getAuthentication().getName();
 		return "health sucess " + email;
+	}
+
+	@Operation(summary = "cicd 작동 여부 확인을 위한 테스트")
+	@GetMapping("/health/cicd")
+	public String cicdCheck() {
+		return "Wow 이게 되네";
 	}
 
 }
