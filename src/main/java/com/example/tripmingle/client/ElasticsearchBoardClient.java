a738020917b1e14dc904953a90be3ff1a8461a6d
송현성 <129783824+thdgustjd1@users.noreply.github.com>
2024-08-01 00:34:00 +0900

    hotfix : feign client 관련된 문제 해결 (#77)

--- src/main/java/com/example/tripmingle/client/ElasticsearchBoardClient.java
+++ src/main/java/com/example/tripmingle/client/ElasticsearchBoardClient.java
@@ -1,22 +1,44 @@
 package com.example.tripmingle.client;
 
-import com.example.tripmingle.common.config.elasticsearch.ElasticsearchFeignConfig;
-import com.example.tripmingle.entity.pojo.ElasticsearchResponse;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.Map;
 
-@FeignClient(name = "ElasticsearchClient", url = "${spring.elasticsearch.uris}", configuration = ElasticsearchFeignConfig.class)
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.example.tripmingle.entity.pojo.ElasticsearchResponse;
+
+import feign.auth.BasicAuthRequestInterceptor;
+
+@FeignClient(name = "ElasticsearchClient", url = "${spring.elasticsearch.uris}", configuration = ElasticsearchBoardClient.FeignConfig.class)
 public interface ElasticsearchBoardClient {
 
-    @PostMapping(value = "${spring.elasticsearch.board-url}" + "/_doc/{id}", consumes = "application/json")
-    Map<String, Object> createOrUpdateBoardDocument(@PathVariable("id") String id, @RequestBody Map<String, Object> document);
+	@PostMapping(value = "${spring.elasticsearch.board-url}" + "/_doc/{id}", consumes = "application/json")
+	Map<String, Object> createOrUpdateBoardDocument(@PathVariable("id") String id,
+		@RequestBody Map<String, Object> document);
 
-    @DeleteMapping(value = "${spring.elasticsearch.board-url}" + "/_doc/{id}")
-    void deleteBoardDocument(@PathVariable("id") String id);
+	@DeleteMapping(value = "${spring.elasticsearch.board-url}" + "/_doc/{id}")
+	void deleteBoardDocument(@PathVariable("id") String id);
 
-    @PostMapping(value = "${spring.elasticsearch.board-url}" + "/_search", consumes = "application/json")
-    ElasticsearchResponse searchBoardDocument(@RequestBody Map<String, Object> query);
+	@PostMapping(value = "${spring.elasticsearch.board-url}" + "/_search", consumes = "application/json")
+	ElasticsearchResponse searchBoardDocument(@RequestBody Map<String, Object> query);
+
+	class FeignConfig {
+
+		@Value("${spring.elasticsearch.username}")
+		private String username;
+
+		@Value("${spring.elasticsearch.password}")
+		private String password;
+
+		@Bean
+		public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
+			return new BasicAuthRequestInterceptor(username, password);
+		}
+	}
 
 }
