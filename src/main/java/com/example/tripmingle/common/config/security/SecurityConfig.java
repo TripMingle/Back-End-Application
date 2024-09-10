f1bfdfe2986b5d75091b79b8b65fd4c6233d103a
mingyu <kimmingyu9811@gmail.com>
2024-09-10 14:28:19 +0900

    chore: 프론트엔드 로컬 환경 CORS 해결

--- src/main/java/com/example/tripmingle/common/config/security/SecurityConfig.java
+++ src/main/java/com/example/tripmingle/common/config/security/SecurityConfig.java
@@ -73,6 +73,7 @@ public class SecurityConfig {
 						CorsConfiguration configuration = new CorsConfiguration();
 
 						configuration.setAllowedOrigins(Collections.singletonList(allowedOrigins));
+						configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
 						configuration.setAllowedOrigins(Collections.singletonList(allowedRemoteOrigins));
 
 						configuration.setAllowedMethods(Collections.singletonList("*"));
