899270cd03a65a2b7f2b26ad22874406ad822837
mingyu <kimmingyu9811@gmail.com>
2024-09-10 18:40:06 +0900

    chore: CORS 설정 누락부분 수정

--- src/main/java/com/example/tripmingle/common/config/security/SecurityConfig.java
+++ src/main/java/com/example/tripmingle/common/config/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.tripmingle.common.config.security;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -71,9 +72,11 @@ public class SecurityConfig {
 					@Override
 					public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 						CorsConfiguration configuration = new CorsConfiguration();
-
-						configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-						configuration.setAllowedOrigins(Collections.singletonList(allowedRemoteOrigins));
+						
+						configuration.setAllowedOrigins(Arrays.asList(
+							allowedOrigins,
+							allowedRemoteOrigins
+						));
 
 						configuration.setAllowedMethods(Collections.singletonList("*"));
 						configuration.setAllowCredentials(true);
