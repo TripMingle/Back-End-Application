b60fbef50d85649c6fbbaa00d803c2f3892b22a7
songhyeonseong <shsum7@gmail.com>
2024-10-11 14:54:19 +0900

    [SWM-113] feat : redisson을 사용한 분산락 추가구

--- src/main/java/com/example/tripmingle/application/service/CompanionService.java
+++ src/main/java/com/example/tripmingle/application/service/CompanionService.java
@@ -1,7 +1,11 @@
 package com.example.tripmingle.application.service;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import com.example.tripmingle.port.out.CacheManagerPort;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -17,11 +21,15 @@ import com.example.tripmingle.port.out.CompanionPersistPort;
 
 import lombok.RequiredArgsConstructor;
 
+import static com.example.tripmingle.common.constants.Constants.COMPANION_LOCK_KEY;
+
 @Service
 @RequiredArgsConstructor
 public class CompanionService {
 	private final UserUtils userUtils;
 	private final CompanionPersistPort companionPersistPort;
+	private final CacheManagerPort cacheManagerPort;
+	private final RedissonClient redissonClient;
 
 	public void registerLeader(Board board, User user) {
 		Companion companion = Companion.builder()
@@ -34,19 +42,41 @@ public class CompanionService {
 
 	public void confirmUsers(Board board, User currentUser, List<User> userList) {
 		userUtils.validateMasterUser(board.getUser().getId(), currentUser.getId());
+		String lockKey = COMPANION_LOCK_KEY + board.getId();
+		RLock lock = redissonClient.getLock(lockKey);
 
-		userList.forEach(user -> {
-				if (!user.getId().equals(board.getUser().getId())) {
-					Companion companion = Companion.builder()
-						.user(user)
-						.board(board)
-						.position(Position.MEMBER)
-						.build();
-					companionPersistPort.saveCompanion(companion);
+		try {
+			if (lock.tryLock(5, 10, TimeUnit.SECONDS)) {
+				try {
+					long currentCompanionCount = cacheManagerPort.getCompanionCount(board.getId());
+
+					if (currentCompanionCount + userList.size() > board.getMaxCount()) {
+						throw new IllegalStateException("동행자 수가 최대값을 초과했습니다.");
+					}
+
+					userList.stream()
+							.filter(user -> !user.getId().equals(board.getUser().getId()))
+							.filter(user -> !companionPersistPort.existsByUserIdAndBoardId(user.getId(), board.getId()))
+							.forEach(user -> {
+								Companion companion = Companion.builder()
+										.user(user)
+										.board(board)
+										.position(Position.MEMBER)
+										.build();
+								companionPersistPort.saveCompanion(companion);
+							});
+
+					cacheManagerPort.incrementCompanionCount(board.getId(), (long)userList.size());
+
+				} finally {
+					lock.unlock();
 				}
+			} else {
+				throw new IllegalStateException("락 획득 실패");
 			}
-		);
-
+		} catch (InterruptedException e) {
+			throw new IllegalStateException("락 획득 중 인터럽트 발생", e);
+		}
 	}
 
 	public void leaveCompanion(Board board, User currentUser) {
