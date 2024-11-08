1aff255239d978068340fb1c5e38556b72cbf1d3
MinGyu <kimmingyu9811@gmail.com>
2024-11-08 18:02:37 +0900

    feat: 유저 프로필 조회 완료 (#99)

--- src/main/java/com/example/tripmingle/adapter/in/UserController.java
+++ src/main/java/com/example/tripmingle/adapter/in/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,6 +16,7 @@ import com.example.tripmingle.common.result.ResultCode;
 import com.example.tripmingle.common.result.ResultResponse;
 import com.example.tripmingle.dto.req.user.PatchUserMyPageReqDTO;
 import com.example.tripmingle.dto.res.user.GetUserInfoResDTO;
+import com.example.tripmingle.dto.res.user.GetUserProfileResDTO;
 import com.example.tripmingle.dto.res.user.PatchUserMyPageResDTO;
 import com.example.tripmingle.dto.res.user.UploadUserImageResDTO;
 import com.example.tripmingle.port.in.UserUseCase;
@@ -45,11 +47,18 @@ public class UserController {
 		return ResponseEntity.ok(ResultResponse.of(ResultCode.UPDATE_USER_MY_PAGE_SUCCESS, patchUserMyPageResDTO));
 	}
 
-	@Operation(summary = "로그인 유저 정보 조회")
+	@Operation(summary = "로그인(접속 중인) 유저 정보 조회")
 	@GetMapping("/info")
 	public ResponseEntity<ResultResponse> getUserInfo() {
 		GetUserInfoResDTO getUserInfoDTO = userUseCase.getUserInfo();
 		return ResponseEntity.ok(ResultResponse.of(ResultCode.GET_USER_INFO_SUCCESS, getUserInfoDTO));
+	}
+
+	@Operation(summary = "유저 프로필 조회")
+	@GetMapping("/my-page/profile")
+	public ResponseEntity<ResultResponse> getUserProfile(@RequestParam("userId") Long userId) {
+		GetUserProfileResDTO getUserProfileResDTO = userUseCase.getUserProfile(userId);
+		return ResponseEntity.ok(ResultResponse.of(ResultCode.GET_USER_PROFILE_SUCCESS, getUserProfileResDTO));
 	}
 
 }
